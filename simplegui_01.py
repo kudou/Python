ea3aa3bf1736f63dd8c222fecbb9dc315b104061
kudou <hwp4869@163.com>
2014-04-01 16:18:12 +0800

    创建简单GUI程序的七步骤

---
+++ simplegui_01.py
@@ -0,0 +1,35 @@
+# simpleGUI program template
+
+# Import the module
+import simplegui
+
+# Define global variables (program state)
+counter = 0
+
+# Define "helper" functions
+def increment():
+    global counter
+    counter += 1
+    
+# Define event handler functions
+def tick():
+    increment()
+    print counter
+    
+def buttonpress():
+    global counter
+    counter = 0
+    
+# Create a frame
+frame = simplegui.create_frame("SimpleGUI Test", 100, 100)
+
+# Register event handler
+
+timer = simplegui.create_timer(1000, tick)
+frame.add_button("Click me!", buttonpress)
+
+# Start frame and timers
+
+frame.start()
+timer.start()
+
