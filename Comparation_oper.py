81e99ac378a68a60319fd42da3fff580eb7cae1f
kudou <hwp4869@163.com>
2014-04-01 11:10:00 +0800

    增加了比较运算符

---
+++ Comparation_oper.py
@@ -0,0 +1,24 @@
+# Comparation Operators
+
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+a = 7 > 3
+print a
+
+x = 8
+y = 5
+b = x >= y
+print b
+
+c = "hello `ls`" == 'hello `ls`'
+print c
+
+d = 20.6 <= 18.3
+print d
+
+print (a and b) or (a and (not d))
