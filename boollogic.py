6815f621cebd02f75cc619de27fa94d1418eb33b
kudou <hwp4869@163.com>
2014-04-01 11:09:30 +0800

    增加了逻辑预算符

---
+++ boollogic.py
@@ -0,0 +1,18 @@
+a = True
+b = False
+c = True
+d = False
+
+print a
+print b
+
+print "===="
+
+print not a
+print a and b
+print a or b
+print not b
+
+print "===="
+
+print (a and b) or (c and (not d))
