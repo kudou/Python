4411cb0b4791482add8ba82862d5cd1a3ab05ffd
kudou <hwp4869@163.com>
2014-04-01 09:50:03 +0800

    Python中关于算数运算

---
+++ Aritnmetic_ex.py
@@ -0,0 +1,12 @@
+# 算数
+print 3, -1, 3.14, -2.8
+
+print int(3.2), int(-3.1)
+
+print float(3), float(-5)
+
+print 1 + 2, 3 - 4, 5 * 6, 2 ** 5
+print 1 / 3, float(5) / 2, float(-7) / 3
+print 1 // 3, 5 // 2, -7 // 3
+
+print 1 + 2 ** 3, 4 -5 /6, 7 * 8 + 9 * 10
