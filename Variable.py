e185ae1b24dca2a336fd9899c0a93806d54acb53
kudou <hwp4869@163.com>
2014-04-01 10:16:06 +0800

    Python变量名与C语言类似

---
+++ Variable.py
@@ -0,0 +1,29 @@
+# Variables nameing and use
+my_name = "hello"
+print my_name
+
+my_age = 25
+print my_age
+
+my_age = my_age + 1
+
+print my_age
+
+magic_pill = 30
+
+print my_age - magic_pill
+
+my_grand_dad = 74
+
+my_grand_dad -= 2 * magic_pill
+
+print my_grand_dad
+
+
+temp_fahrenheit = 212
+temp_celsius = float(5) / 9 *(temp_fahrenheit - 32)
+print temp_celsius
+
+temp_celsius = 100 
+temp_fahrenheit = float(9)/5 * temp_celsius  + 32
+print temp_fahrenheit
